1ab5452717e007592fbf206402c8122450b819af
Inderpreet Singh Chera <inderchera@gmail.com>
2020-01-29 22:10:41 +0530

    Code completed(time calculations left)

--- assign2/addNumbersRecursive.c
+++ assign2/addNumbersRecursive.c
@@ -1,9 +1,106 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv){
-	
+	int my_id, size, n, no_per_process, sum=0, levels=0;
+	MPI_Status status;
+	int arr[1000];
+	int arr1[1000];
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	n=atoi(argv[1]);
+	no_per_process = n/size;
+	int r=size;
+	while(r!=0){
+		levels++;
+		r=r>>1;
+	}
+	levels--;
+	int root_process = 0;
 
-    
+	if(my_id == root_process){
+		for(int i=0;i<n;i++){
+			arr[i] = i+1;
+		}
+		
+		int templevel=levels;
+		// templevel--;
+		int start=n;
+		int pid = size;
+		while(templevel>0){
+			templevel--;
+			start = start >> 1;
+        	pid = pid>>1;
+        	MPI_Send(&arr[start], start, MPI_INT, pid, 0, MPI_COMM_WORLD);
+		}
+		// for(int i=0;i<no_per_process;i++){
+		// 	sum+=
+		// }
+	}
+	else{
+		int get_level=0;
+		int id = my_id;
+		while(id%2==0){
+			get_level++;
+			id = id>>1;
+		}
+		get_level = levels - get_level;
+		// printf("id: %d, level: %d\n",my_id, get_level );
+		MPI_Recv(arr1,n/(1<<(get_level)), MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+		// printf("id: %d, level: %d\n",my_id, get_level );
+
+		int start = n/(1<<(get_level));
+		int temp= levels-get_level;
+		while(temp){
+			temp--;
+			start = start>>1;
+        	// printf("%d sending to %d\n", my_id, my_id + (1<<temp));
+        	MPI_Send(&arr1[start], start, MPI_INT, my_id+(1<<temp), 0, MPI_COMM_WORLD);
+			// get_level++;
+			// temp--;
+		}
+		// printf("%d %d %d\n",arr1[0], arr1[1], my_id );
+
+	}
+
+	if(my_id == root_process){
+		for(int i=0;i<no_per_process;i++){
+			sum+=arr[i];
+		}
+		int id = my_id;
+		int templevel = 0;
+		while(templevel<levels){
+	// printf("test\n");
+			int partial_sum=0;
+			MPI_Recv(&partial_sum, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+			templevel++;
+			sum += partial_sum;
+		}
+			printf("%d\n", sum);
+	}	
+	else{
+		int partial_sum=0;
+		for(int i=0;i<no_per_process;i++){
+			partial_sum+=arr1[i];
+		}
+		int id = my_id;
+		int templevel=0;
+		while(id%2==0){
+			templevel++;
+			id = id>>1;
+		}
+		int tl = templevel;
+		while(templevel>0){
+			int psum=0;
+			MPI_Recv(&psum, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+			partial_sum += psum;
+			templevel--;
+		}
+		// printf("%d sending to %d\n",my_id, my_id -(1<<tl) );
+        MPI_Send(&partial_sum, 1, MPI_INT, my_id-(1<<tl), 0, MPI_COMM_WORLD);
+	}
+	MPI_Finalize();
 	return 0;
 }
--- assign2/addNumersReduce.c
+++ assign2/addNumersReduce.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv){
-	int my_id, size, n, no_per_process, sum=0;
+	int my_id, size, n, no_per_process, sum=0, partial_sum=0;
 	MPI_Status status;
 	// printf("please enter the number of numbers to sum: ");
  //    scanf("%i", &n);
@@ -26,39 +26,23 @@ int main(int argc, char** argv){
         	int start=i*no_per_process;
         	MPI_Send(&arr[start], no_per_process, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
-
+        for(int i=0;i<no_per_process;i++){
+        	partial_sum += arr[i];
+        }
         
 	}
 	else{
 		MPI_Recv(&arr1, no_per_process, MPI_INT, root_process, 0, MPI_COMM_WORLD, &status);
-	}
-	
-
-	if(my_id == root_process){
 		for(int i=0;i<no_per_process;i++){
-			sum+=arr[i];
-		}
-
-		for(int i=1;i<size;i++){
-			int partial_sum=0;
-			MPI_Recv(&partial_sum, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-
-			//debug statements
-			// sender = status.MPI_SOURCE;
-            // printf("Partial sum %i returned from process %i\n", partial_sum, sender);
-
-            sum+=partial_sum;
-		}
-		printf("Final Sum: %d\n", sum);
-	}
-	else{
-		int partial_sum=0;
-		for(int i=0;i<no_per_process;i++){
-			partial_sum += arr1[i];
-		}
-		MPI_Send(&partial_sum, 1, MPI_INT, root_process, 0, MPI_COMM_WORLD);
+        	partial_sum += arr1[i];
+        }
 	}
 
-	MPI_Finalize();
+	MPI_Reduce(&partial_sum, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    
+    if(my_id == 0){
+    	printf("Total Sum: %d\n", sum);
+    }
+    MPI_Finalize();
 	return 0;
 }
