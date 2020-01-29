46a2279ba6d9544d966732e1d97f0d5f07422159
Inderpreet Singh Chera <inderchera@gmail.com>
2020-01-30 00:45:47 +0530

    Assignment 1 completed

--- assign1/run.sh
+++ assign1/run.sh
@@ -1,10 +1,32 @@
-g++ opt2.cpp
-./a.out 32 64 >> opt2_s_64.txt
-./a.out 64 64 >> opt2_s_64.txt
-./a.out 128 64 >> opt2_s_64.txt
-./a.out 256 64 >> opt2_s_64.txt
-./a.out 512 64 >> opt2_s_64.txt
-./a.out 1024 64 >> opt2_s_64.txt
-./a.out 2048 64 >> opt2_s_64.txt
-./a.out 4096 64 >> opt2_s_64.txt
-./a.out 8192 64 >> opt2_s_64.txt
+echo "Naive ka chl rha"
+g++ -O1 Naive.cpp
+./a.out 1024 64
+g++ -O2 Naive.cpp
+./a.out 1024 64
+g++ -O3 Naive.cpp
+./a.out 1024 64
+
+
+echo "Opt1 ka chl rha"
+g++ -O1 opt1.cpp
+./a.out 1024 64
+g++ -O2 opt1.cpp
+./a.out 1024 64
+g++ -O3 opt1.cpp
+./a.out 1024 64
+
+echo "Opt2 ka chl rha"
+g++ -O1 opt2.cpp
+./a.out 1024 64
+g++ -O2 opt2.cpp
+./a.out 1024 64
+g++ -O3 opt2.cpp
+./a.out 1024 64
+
+echo "Opt3 ka chl rha"
+g++ -O1 opt3.cpp
+./a.out 1024 64
+g++ -O2 opt3.cpp
+./a.out 1024 64
+g++ -O3 opt3.cpp
+./a.out 1024 64
