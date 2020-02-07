2311a22c9d4079bcd40e9568c1ab46d30901a34c
Inderpreet Singh Chera <inderchera@gmail.com>
2020-02-07 19:02:51 +0530

    Added oddEvenSorting.c to assignment 3

---
+++ assign3/oddEvenSorting.c
@@ -0,0 +1,120 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void merge(int l, int mid, int h, int* arr){
+	int* a = (int*)malloc(sizeof(int)*(mid-l+1));
+	int* b = (int*)malloc(sizeof(int)*(h-mid+1));
+	int x=0,y=0;
+	for(int i=l;i<=mid;i++){
+		a[x++] = arr[i];
+	}
+	for(int i=mid+1;i<=h;i++){
+		b[y++] = arr[i];
+	}
+	int j=0,k=0;
+	for(int i=l;i<=h;i++){
+		if(j<x && k<y){
+			if(a[j]<=b[k]){
+				arr[i] = a[j++];
+			}
+			else{
+				arr[i] = b[k++];
+			}
+		}
+		else if(j<x){
+			arr[i] = a[j++];
+		}
+		else{
+			arr[i] = b[k++];
+		}
+	}
+}
+
+void mergesort(int l, int h, int* arr){
+	if(h>l){
+		int mid=(l+h)/2;
+		mergesort(l, mid, arr);
+		mergesort(mid+1, h, arr);
+		merge(l, mid, h, arr);
+	}
+}
+
+int main(int argc, char** argv){
+    int my_id, size;
+
+    int n = atoi(argv[1]);
+
+    MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	int no_per_process = n/size;
+	int root_process = 0;
+
+	int* arr = (int*)malloc(sizeof(int)*n);
+	int* arr1 = (int*)malloc(sizeof(int)*no_per_process*2);
+
+	if(my_id == root_process){
+		for(int i=0;i<n;i++){
+			arr[i] = n-i;
+		}
+		// mergesort(0, n-1, arr);
+		// for(int i=0;i<n;i++){
+		// 	printf("%d ", arr[i]);
+		// }
+		// printf("\n");
+	}
+
+	MPI_Scatter(arr, no_per_process, MPI_INT, arr1, no_per_process, MPI_INT, root_process, MPI_COMM_WORLD);
+
+	mergesort(0, no_per_process, arr1);
+
+	for(int i=0;i<no_per_process;i+=2){
+		if(my_id%2==0 && my_id+1<size){
+			MPI_Recv(&arr1[no_per_process], no_per_process, MPI_INT, my_id+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			merge(0, no_per_process-1,2*no_per_process-1,arr1);
+		}
+		else if(my_id%2==1 && my_id-1>=0){
+        	MPI_Send(arr1, no_per_process, MPI_INT, my_id-1, 0, MPI_COMM_WORLD);
+		}
+		if(my_id%2==0 && my_id+1<size){
+        	MPI_Send(&arr1[no_per_process], no_per_process, MPI_INT, my_id+1, 0, MPI_COMM_WORLD);
+		}
+		else{
+			MPI_Recv(arr1, no_per_process, MPI_INT, my_id-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+
+		MPI_Barrier(MPI_COMM_WORLD);
+		
+		if(my_id%2==1 && my_id+1<size){
+			MPI_Recv(&arr1[no_per_process], no_per_process, MPI_INT, my_id+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			merge(0, no_per_process-1,2*no_per_process-1,arr1);
+		}
+		else if(my_id%2==0 && my_id-1>=0){
+        	MPI_Send(arr1, no_per_process, MPI_INT, my_id-1, 0, MPI_COMM_WORLD);
+		}
+		if(my_id%2==1 && my_id+1<size){
+        	MPI_Send(&arr1[no_per_process], no_per_process, MPI_INT, my_id+1, 0, MPI_COMM_WORLD);
+		}
+		else if(my_id%2==0 && my_id-1>=0){
+			MPI_Recv(arr1, no_per_process, MPI_INT, my_id-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
+
+	if(root_process == my_id){
+		// printf("%d\n", my_id);
+		MPI_Gather(arr1, no_per_process, MPI_INT, arr, no_per_process, MPI_INT, root_process, MPI_COMM_WORLD);
+		for(int i=0;i<n;i++){
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+	}
+	else{
+		MPI_Gather(arr1, no_per_process, MPI_INT, NULL, 0, MPI_INT, root_process, MPI_COMM_WORLD);
+	}
+	MPI_Finalize();
+
+	return 0;
+}
