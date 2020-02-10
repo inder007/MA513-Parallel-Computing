1335544f2fc4b89edd3ea13bc7ae36d8e3ff0054
Inderpreet Singh Chera <inderchera@gmail.com>
2020-02-10 19:40:48 +0530

    Corrected bug in Assignment 2

--- assign2/unblockingSendReceive.c
+++ assign2/unblockingSendReceive.c
@@ -5,19 +5,23 @@ int main(int argc, char** argv){
 
 	int rank, data[100], size;
 	MPI_Request reqs[2];
-	MPI_Status stats[2];
+	MPI_Status stats;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	if(rank%2 == 0){
 		MPI_Isend(&data, 100, MPI_INT, (rank+1)%size, 0, MPI_COMM_WORLD, &reqs[0]);
+		MPI_Irecv(&data, 100, MPI_INT, (rank-1)%size, 0, MPI_COMM_WORLD, &reqs[1]);
+
 		// printf("send complete of %d\n", rank);
 	}
 	else{
-		MPI_Irecv(&data, 100, MPI_INT, (rank-1)%size, 0, MPI_COMM_WORLD, &reqs[1]);
+		MPI_Isend(&data, 100, MPI_INT, (rank-1)%size, 0, MPI_COMM_WORLD, &reqs[0]);
+		MPI_Irecv(&data, 100, MPI_INT, (rank+1)%size, 0, MPI_COMM_WORLD, &reqs[1]);
 
 	}
-	MPI_Waitall(1, reqs, stats);
+	MPI_Wait(&reqs[0], &stats);
+	MPI_Wait(&reqs[1], &stats);
 	// else if(rank==1){
 	// if(rank%2 == 0){
 	// 	MPI_Recv(data, 100, MPI_INT, (rank+1)%size, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
