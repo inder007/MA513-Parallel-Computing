acefb1407c048c0dfd05bc83ff035481b9f65301
Inderpreet Singh Chera <inderchera@gmail.com>
2020-02-11 19:24:56 +0530

    Assignment 4 code part completed

--- assign4/blockdiv.c
+++ assign4/blockdiv.c
@@ -43,8 +43,13 @@ int main(int argc, char** argv){
 					a[i+n*j] = 0;
 				}
 			}
+			// x[i] = 1;
+			// b[i] = 0;
+		}
+	}
+	if(my_id == p_per_row - 1){
+		for(int i=0;i<n;i++){
 			x[i] = 1;
-			b[i] = 0;
 		}
 	}
 
@@ -65,7 +70,65 @@ int main(int argc, char** argv){
 	// }
 	// printf("\n");
 
-	
+	if(my_row_id == p_per_row-1){
+		// for(int i=0;i<row_per_process;i++){
+		// 	printf("%d-%d ", my_id, x[i]);
+		// }
+		// printf("\n");
+		MPI_Scatter(x, row_per_process, MPI_INT, x, row_per_process, MPI_INT, 0, col_comm);
+	}
+
+	double t1, t2, pt, time, pt2;
+	t1 = MPI_Wtime();
+
+	for(int k=0;k<5;k++){
+		if(my_row_id == p_per_row-1 && my_id != size-1){
+			MPI_Send(x, row_per_process, MPI_INT, my_col_id, 0, row_comm);
+		}
+		if(my_row_id == my_col_id){
+			if(my_id != size-1)MPI_Recv(x, row_per_process, MPI_INT, MPI_ANY_SOURCE, 0, row_comm, MPI_STATUS_IGNORE);
+		}
+		MPI_Bcast(x, row_per_process, MPI_INT, my_row_id, col_comm);
+		// for(int i=0;i<row_per_process;i++){
+		// 	printf("%d-%d ", my_id, x[i]);
+		// }
+		// printf("\n");
+		// printf("%d reached\n", my_id);
+		
+
+		MPI_Barrier(MPI_COMM_WORLD);
+
+		for(int i=0;i<row_per_process;i++){
+			b[i] = 0;
+			for(int j=0;j<row_per_process;j++){
+				b[i] += a[i*row_per_process + j]*x[j];
+			}
+		}
+
+		
+		MPI_Reduce(b, x, row_per_process, MPI_INT, MPI_SUM, p_per_row-1, row_comm);
+
+
+	}
+
+	if(my_row_id == p_per_row-1){
+		MPI_Gather(x, row_per_process, MPI_INT, x, row_per_process, MPI_INT, root_process, col_comm);
+	}
+
+	t2 = MPI_Wtime();
+	MPI_Reduce(&t1, &pt, 1, MPI_DOUBLE, MPI_MIN, root_process, MPI_COMM_WORLD);
+	MPI_Reduce(&t2, &pt2, 1, MPI_DOUBLE, MPI_MAX, root_process, MPI_COMM_WORLD);
+
+	// if(my_id == p_per_row-1){
+	// 	for(int i=0;i<n;i++){
+	// 		printf("%d ", x[i]);
+	// 	}
+	// 	printf("\n");
+	// }
+	if(my_id == root_process){
+		time = pt2 - pt;
+		printf("%lf\n", time);
+	}
 
 
 	MPI_Finalize();
