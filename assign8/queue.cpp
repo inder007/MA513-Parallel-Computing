796347decddb118c1b96933ec9deaca9593ca2e3
Inderpreet Singh Chera <inderchera@gmail.com>
2020-04-26 16:18:47 +0530

    Code almost completed

--- assign8/queue.cpp
+++ assign8/queue.cpp
@@ -3,19 +3,29 @@
 
 using namespace std;
 
+
 struct node;
 
 struct pointer{
 	node* ptr;
 	int count;
+	int nodeNum;
 
-	pointer() : ptr{NULL}, count{0} {}
-	pointer(node* _ptr) : ptr{_ptr}, count{0} {}
-	pointer(node* _ptr, int _count) :  ptr{_ptr}, count{_count} {}
+	pointer() : ptr{NULL}, count{0}, nodeNum{0} {}
+	pointer(node* _ptr) : ptr{_ptr}, count{0}, nodeNum{0} {}
+	pointer(node* _ptr, int _count, int _nodeNum) :  ptr{_ptr}, count{_count}, nodeNum{_nodeNum} {}
 
 	bool operator ==(const pointer &other) const {
-        return ptr == other.ptr && count == other.count;
-    }    
+        return ptr == other.ptr && count == other.count && nodeNum == other.nodeNum;
+    }
+
+    bool operator !=(const pointer &other) const {
+    	if(ptr != other.ptr || count != other.count || nodeNum != other.nodeNum){
+    		return 1;
+    	}
+    	return 0;
+        // return ptr != other.ptr || count != other.count || nodeNum != other.nodeNum;
+    }        
 };
 
 
@@ -35,27 +45,39 @@ struct myqueue{
 };
 
 
-void enqueue(myqueue *q, int value){
+int maxQueueSize;
+myqueue* q;
+int numOfJobsPerProducer;
+
+
+bool enqueue(myqueue *q, int value){
 	node* temp = new node(value);
 	// temp->value = value;
 	// temp->next.ptr = NULL;
 	pointer tail;
 	while(1){
 		tail = q->tail.load();
+		pointer head = q->head.load();
+		if(tail.nodeNum - head.nodeNum >= maxQueueSize){
+			// cout<<tail.count - head.count<<endl;
+			free(temp);
+			return 0;
+		}
 		pointer next = tail.ptr->next;
 		if(tail == q->tail){
 			if(next.ptr == NULL){
-				if(tail.ptr->next.compare_exchange_weak(next, pointer{temp, next.count+1})){
+				if(tail.ptr->next.compare_exchange_weak(next, pointer{temp, next.count+1, tail.nodeNum+1})){
 					break;
 				}
 			}
 			else{
-				q->tail.compare_exchange_weak(tail, pointer{next.ptr, tail.count+1});
+				q->tail.compare_exchange_weak(tail, pointer{next.ptr, tail.count+1, tail.nodeNum+1});
 			}
 		}
 	}
 
-	q->tail.compare_exchange_weak(tail, pointer{temp, tail.count+1});
+	q->tail.compare_exchange_weak(tail, pointer{temp, tail.count+1, tail.nodeNum+1});
+	return 1;
 }
 
 
@@ -70,11 +92,11 @@ bool dequeue(myqueue* q, int* value){
 				if(next.ptr == NULL){
 					return 0;
 				}
-				q->tail.compare_exchange_weak(tail, pointer{next.ptr, tail.count+1});
+				q->tail.compare_exchange_weak(tail, pointer{next.ptr, tail.count+1, tail.nodeNum+1});
 			}
 			else{
 				*value = next.ptr->value;
-				if(q->head.compare_exchange_weak(head, pointer{next.ptr, head.count+1})){
+				if(q->head.compare_exchange_weak(head, pointer{next.ptr, head.count+1, next.nodeNum})){
 					break;
 				}
 			}
@@ -84,23 +106,73 @@ bool dequeue(myqueue* q, int* value){
 	return 1;
 }
 
+void printQueue(){
+	pointer head = q->head.load();
+	// pointer tail = q->tail.load();
+	// cout<<"chec"<<endl;
+	while(head.ptr != NULL){
+		cout<<head.nodeNum<<" ";
+		head = head.ptr->next;
+	}
+	cout<<endl;
+}
+
+void* producerWork(void* id){
+	int* tid = (int*)id;
+	for(int i=0;i<numOfJobsPerProducer;i++){
+		int task = (*tid)%2 + 1;
+		// while(!enqueue(q,task));
+		enqueue(q, task);
+	}
+}
+
+void* consumerWork(void* t){
+	while(1){
+		int ans;
+		if(!dequeue(q, &ans)){
+			break;
+		}
+		this_thread::sleep_for(chrono::microseconds(ans));
+	}
+}
+
+
 int main(int argc, char** argv){
 	
-	myqueue* q = new myqueue;
+	q = new myqueue;
 	
-	// int numProducers = argv[1];
-	// int numConsumers = argv[2];    
+	int numProducers = atoi(argv[1]);
+	int numConsumers = atoi(argv[2]);
+	maxQueueSize = atoi(argv[3]);
+	numOfJobsPerProducer = atoi(argv[4]);
 
-	enqueue(q, 1);
-	enqueue(q, 2);
-	enqueue(q, 3);
-	enqueue(q, 4);
-	int ans;
-	cout<<q->tail.load().count<<endl;
-	dequeue(q, &ans);
-	enqueue(q, 5);
-	cout<<q->tail.load().count<<endl;
-	dequeue(q, &ans);
-	cout<<q->tail.load().count<<endl;
+	pthread_t producers[numProducers];
+	pthread_t consumers[numConsumers];
+
+	int threadIds[numProducers];
+
+	for(int i=0;i<numProducers;i++){
+		threadIds[i] = i;
+		pthread_create(&producers[i], NULL, producerWork, (void*)&threadIds[i]);
+	}
+
+	for(int i=0;i<numConsumers;i++){
+		pthread_create(&consumers[i], NULL, consumerWork, NULL);
+	}
+
+	for(int i=0;i<numProducers;i++){
+		pthread_join(producers[i], NULL);
+	}
+
+	// int ans;
+	// dequeue(q, &ans);
+	// dequeue(q, &ans);
+
+	for(int i=0;i<numConsumers;i++){
+		pthread_join(consumers[i], NULL);
+	}
+
+	printQueue();
+
 	return 0;
 }
